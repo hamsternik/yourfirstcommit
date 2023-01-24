46cc0b8fcd907804651acf80fcc458c1d78b653a
Ress <mail@ress.ws>
2023-01-24 10:59:08 +0100

    App setup, basic UI elements added

---
+++ YourFirstCommit/YourFirstCommit/Views/RepoNameField.swift
@@ -0,0 +1,8 @@
+//
+//  RepoNameField.swift
+//  YourFirstCommit
+//
+//  Created by Alex Antipov on 24.01.2023.
+//
+
+import Foundation
---
+++ YourFirstCommit/YourFirstCommit/YourFirstCommitApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourFirstCommitApp.swift
+//  YourFirstCommit
+//
+//  Created by Alex Antipov on 24.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct YourFirstCommitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
