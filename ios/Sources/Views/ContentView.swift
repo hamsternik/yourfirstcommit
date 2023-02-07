0deb00b07fbdd78e428acab34fd9c282471c1005
Nikita Khomitsevych <hamsternik9@gmail.com>
2023-02-08 00:40:25 +0200

    update first screen content view

--- ios/Sources/Views/ContentView.swift
+++ ios/Sources/Views/ContentView.swift
@@ -16,26 +16,34 @@ struct ContentView_Previews: PreviewProvider {
 struct ContentView: View {
     var body: some View {
         NavigationView {
-            GeometryReader { geometry in
-                VStack(alignment: .leading, spacing: 15) {
-                    Text("Your First Commit")
-                        .font(.title)
-                        .padding(.bottom)
-                    
-                    // 1.. typing repo name
-                    
-                    RepoNameField()
-                    
-                    // 2.. getting files listing of initial commit
-                    
-                    // 3.. profit!
-                    
-                    
-                }
+            contentView
                 .padding()
-                .frame(width: geometry.size.width, alignment: .leading)
-            }
+                .navigationTitle("Your First Commit")
+                .navigationBarTitleDisplayMode(.automatic)
         }
-        .navigationTitle("Your First Commit")
+    }
+    
+    private var contentView: some View {
+        VStack(alignment: .leading, spacing: 15) {
+            centeredContentView
+            // 1.. typing repo name
+            RepoNameField()
+            // 2.. getting files listing of initial commit
+            // 3.. profit!
+        }
+    }
+    
+    private var centeredContentView: some View {
+        VStack {
+            Spacer()
+            Text("""
+            Onboarding text here.
+            Let's tell our users what the app is about.
+            Need to figure out what the impressive copy should be there.
+            Maybe we need to add a simple instruction / steps to follow user how the app works.
+            """)
+                .multilineTextAlignment(.center)
+            Spacer()
+        }
     }
 }
